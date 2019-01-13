b4a2e6ebc21951bd09f3922c0b872b25f8897ba4
Harley Santos Garzón <harley.santos.garzon@gmail.com>
2019-01-13 17:40:59 -0500

    Refactor component

--- src/Service/FetchComponent.js
+++ src/Service/FetchComponent.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { Component } from 'react';
 
-const FetchComponent = () => (
-    <div>
-        Hello World!
-    </div>
-);
+/**
+ * 
+ */
+
+const FetchComponent = Wrapper => class Feth extends Component {
+    constructor() {
+        super();
+        this.state = { result: null }
+    }
+
+    handleNewFetchWithNewProps = props => {
+        fetch(props)
+            .then(r => r.json())
+            .then(result => this.setState({ result }));
+    };
+
+    componentDidMount() {
+        this.handleNewFetchWithNewProps(this.props.url);
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if(this.props.url !== prevProps.url) {
+            this.handleNewFetchWithNewProps(this.props.url);
+        }
+    }
+
+    render() {
+        const { result } = this.state;
+        return result ? <Wrapper data={result}/> : null
+    }
+};
 
 export default FetchComponent;
