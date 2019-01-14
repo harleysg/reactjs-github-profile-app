67e2671f6be2746381113b970926cd717f99c915
Harley Santos Garzón <santo.gh@gmail.com>
2019-01-14 09:41:32 -0500

    update: nuevo componente y metodo

--- src/components/GitHubProfile.js
+++ src/components/GitHubProfile.js
@@ -1,12 +1,28 @@
 import React, { Component, Fragment } from "react";
-import ElementUI from './UI/ElementUI'
+import ElementUI from './UI/ElementUI';
+import SearchProfile from './SearchProfile'
 
 class GithubProfile extends Component {
+
+    setStateWithUsername(username) {
+        console.log(username);
+        
+    }
+    // setNewUrlAPI = (username) => {
+    //     return `${this.urlAPI}/${username}`
+    // };
+
+    // setStateAfterSearch(username) {
+        // let url = this.setNewUrlAPI(username);
+        // this.setState({ username: username, url: url });
+    // }
+
     render() {
         return (
             <Fragment>
-                <ElementUI Wrapper='header' typeClass='o' shadowSize={2}>
+                <ElementUI Wrapper='header' className="u-flex-around-center" nameClass="header" typeClass='o' shadowSize={2}>
                     <div>Github profile</div>
+                    <SearchProfile handleFunction={this.setStateWithUsername.bind(this)}/>
                 </ElementUI>
             </Fragment>
         )
